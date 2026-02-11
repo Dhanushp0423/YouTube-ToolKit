917bc26baa492430597dcf6996ac983b8b4ca320
Dhanushp0423 <dhanushp0408@gmail.com>
2026-02-11 11:18:47 +0530

    Update CorsConfig.java
    Final Commit of Corsfig .java

--- youtube-toolkit/src/main/java/com/youtubetoolkit/config/CorsConfig.java
+++ youtube-toolkit/src/main/java/com/youtubetoolkit/config/CorsConfig.java
@@ -1,27 +1,28 @@
 package com.youtubetoolkit.config;
 
-
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-  @Value("{cors.allowed-origins}")
-    private String[] allowedOrigins;
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(
+                List.of("https://youtube-toolkit-frontend.vercel.app")
+        );
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
 
-  @Override
-    public void addCorsMappings(CorsRegistry registry){
-
-      registry.addMapping("/api/**")
-              .allowedOrigins(allowedOrigins)
-              .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-              .allowedHeaders("*")
-              .allowCredentials(false)
-              .maxAge(3600);
-  }
-
-
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
